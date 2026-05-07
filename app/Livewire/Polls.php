5cea3ccf2682798c742c883ef0499f14e5883a07
Turnionz <turn0nz@gmail.com>
2026-05-07 21:40:18 +0300

    Votes support

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,16 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use App\Models\Poll;
+use Illuminate\Http\Request;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Polls extends Component
 {
+    use WithPagination;
+
     protected $listeners = ['pollCreated' => 'render'];
 
-    public function render()
+    public function render(Request $request)
     {
         $polls = Poll::with('options.votes')->latest()->paginate(3);
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote($optionId)
+    {
+        $option = Option::findOrFail($optionId);
+        $option->votes()->create();
     }
 }
