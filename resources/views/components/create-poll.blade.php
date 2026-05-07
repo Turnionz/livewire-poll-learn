31bd0fd3782d9235ab41fbc089453fa9b12b2583
Turnionz <turn0nz@gmail.com>
2026-05-07 19:59:34 +0300

    Option adding

---
+++ resources/views/components/create-poll.blade.php
@@ -0,0 +1,46 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public $title = '';
+    public $options = ['First'];
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+};
+?>
+
+<div>
+    <form>
+        <label>Poll title</label>
+        <input type="text" wire:model.live="title" />
+
+        Current title: {{ $title }}
+
+        <div class="mt-4 mb-4">
+            <button class="btn" wire:click.prevent="addOption">Add option</button>
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    <label>Option {{ $index + 1 }}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}"/>
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </form>
+</div>
