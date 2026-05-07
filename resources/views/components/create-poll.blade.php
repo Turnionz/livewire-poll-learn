07983e08a944da7627ae59a893dfb63c96eb4bff
Turnionz <turn0nz@gmail.com>
2026-05-07 20:42:34 +0300

    Validation of requests

--- resources/views/components/create-poll.blade.php
+++ resources/views/components/create-poll.blade.php
@@ -8,6 +8,16 @@ new class extends Component
     public $title = '';
     public $options = [''];
 
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:2|max:10',
+        'options.*' => 'required|min:1|max:255'
+    ];
+
+    protected $messages = [
+        'options.*' => "The option can't be empty"
+    ];
+
     public function addOption()
     {
         $this->options[] = '';
@@ -19,8 +29,15 @@ new class extends Component
         $this->options = array_values($this->options);
     }
 
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function createPoll()
     {
+        $this->validate();
+
         Poll::create([
             'title' => $this->title
         ])->options()->createMany(
@@ -39,6 +56,10 @@ new class extends Component
         <label>Poll title</label>
         <input type="text" wire:model.live="title" />
 
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mt-4 mb-4">
             <button class="btn" wire:click.prevent="addOption">Add option</button>
         </div>
@@ -51,6 +72,9 @@ new class extends Component
                         <input type="text" wire:model="options.{{ $index }}"/>
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
